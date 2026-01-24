f317469c9ea7c674413adc36bea934e462ddc69a
Haitao Pan <manbuzhe2009@qq.com>
2026-01-24 11:23:24 +0800

    content: update homepage copy and metadata to 'View & World'

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -55,7 +55,7 @@ export default async function Image() {
                             textAlign: 'center',
                         }}
                     >
-                        Walking with a Camera
+                        View & World
                     </div>
 
                     {/* Tagline */}
