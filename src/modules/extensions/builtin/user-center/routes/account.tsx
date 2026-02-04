37b448680055d2b83873540408b82e5ab17bb2e1
Haitao Pan <manbuzhe2009@qq.com>
2026-02-04 15:21:40 +0800

    feat: hide mfa and billing panels for demo readonly account

--- src/modules/extensions/builtin/user-center/routes/account.tsx
+++ src/modules/extensions/builtin/user-center/routes/account.tsx
@@ -1,13 +1,19 @@
+'use client'
+
 import MfaSetupPanel from '../account/MfaSetupPanel'
 import SubscriptionPanel from '../account/SubscriptionPanel'
 import UserOverview from '../components/UserOverview'
+import { useUserStore } from '@lib/userStore'
 
 export default function UserCenterAccountRoute() {
+  const user = useUserStore((state) => state.user)
+  const isDemoReadOnly = Boolean(user?.isReadOnly && user?.email?.toLowerCase() === 'demo@svc.plus')
+
   return (
     <div className="space-y-6">
       <UserOverview hideMfaMainPrompt />
-      <MfaSetupPanel showSummary={false} />
-      <SubscriptionPanel />
+      {!isDemoReadOnly ? <MfaSetupPanel showSummary={false} /> : null}
+      {!isDemoReadOnly ? <SubscriptionPanel /> : null}
     </div>
   )
 }
